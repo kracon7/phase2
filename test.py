47077da98b05d7f35cf5306fc3397290bd139a69
jiacheng <ethancooper007@gmail.com>
2021-03-24 15:06:26 -0500

    finished 2rotation calibration

--- test.py
+++ test.py
@@ -3,22 +3,48 @@ from sympy import *
 
 
 def func(target):
+	# rotation axis e1
 	e1 = np.array([0.0269, -0.7183, -0.6952])
+	# reverse of x_axis vx
 	vx = np.array([-0.8163, -0.4171, 0.3995])
-	c = np.cross(e1, vx)
-	d = e1 @ vx
+	vz = np.array([0.420562588779785, 0.0447626641885372, 0.906158602460734])
+
+	theta = symbols('theta')
+	L = 59.7
 
 	origin = np.array([32.4282, -81.5175, -147.657])
 	V = target - origin
 
-	theta = symbols('theta')
-	expr_1 = vx * cos(theta) + c * sin(theta) + d * e1 * (1-cos(theta))
+	expr_1 = vx * cos(theta) + np.cross(e1, vx) * sin(theta) + e1 @ vx * e1 * (1-cos(theta))
+	eq1 = Eq(expr_1 @ V, L)
+	result = solve(eq1, theta)
 
-	expr_2 = expr_1 @ V
-	e1 = Eq(expr_2, 59.7)
-	result = solve(e1, theta)
+	if len(result) > 0:
+		for th1 in result:
+			angle_1 = np.rad2deg(float(th1))
+			
+			# if valid angle for rotation 1 is found
+			if angle_1 > -1e-2 and angle_1 < 60:
+				theta_1 = float(th1)
 
-	print(result)
+				# keep solving for theta_2
+				rotated_x = np.zeros(3)
+				for i in range(3):
+					rotated_x[i] = expr_1[i].subs(theta, theta_1)
+
+				origin_2 = origin + L * rotated_x
+
+				vector = target - origin_2
+				vector = vector / np.linalg.norm(vector)
+				rotated_z = vz*np.cos(theta_1) + np.cross(e1, vz)*np.sin(theta_1) \
+							+ e1@vz * e1 * (1-np.cos(theta_1))
+
+				angle_2 = np.rad2deg(np.arccos(rotated_z @ vector))
+				if angle_2 >= 0 and angle_2 <= 20:	
+					print([angle_1, angle_2])
+					return [angle_1, angle_2]
+
+	return None
 
 
 # test target position at [0, 0]
