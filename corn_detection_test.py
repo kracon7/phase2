ac1f57d42561437bfc55740d81cbc425d84d26b5
kracon7 <Kracon@1549>
2021-08-24 02:46:26 -0500

    working on pointcloud fitting to find the corridor

--- corn_detection_test.py
+++ corn_detection_test.py
@@ -1,36 +1,23 @@
 import os
+import sys
 import argparse
+import time
+import pickle
+import copy
 import numpy as np
+import datetime as dt
+import open3d as o3d
 import matplotlib.pyplot as plt
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from model import CorridorNet
 
-def compute_point_cloud(depth, K, rgb=None):
-    '''
-    compute the point cloud in the camera frame
-    Input
-        depth -- ndarray of shape (im_h, im_w)
-        K -- camera intrinsics (3, 3)
-    Output
-        pc_cam -- pointcloud in camera frame, (im_h*im_w, 3)
-    '''
-
-    # pixels of image grid
-    im_h, im_w = depth.shape[:2]
-    x, y = np.arange(im_w), np.arange(im_h)
-    xx, yy = np.meshgrid(x, y)
-    points = np.stack([xx, yy], axis=2).reshape(-1,2)
-
-    # project pixels
-    rays = np.insert(points, 2, 1, axis=1) @ np.linalg.inv(K).T
-    pc_cam = rays.reshape(im_h, im_w, 3) * np.expand_dims(depth, axis=-1)  # im_h x im_w x 3
-
-    if rgb is not None:
-        pc_cam = np.concatenate([pc_cam, rgb], axis=2).reshape(-1, 6)
-    else:
-        pc_cam = pc_cam.reshape(-1, 3)
-    
-    return pc_cam
-
+np.random.seed(0)
 
 def points2heightmap(surface_pts, heightmap_size, ws_limits=[[-0.5,0.5],[-0.4,0.25],[0.1, 0.7]]):
 
@@ -58,66 +45,67 @@ def points2heightmap(surface_pts, heightmap_size, ws_limits=[[-0.5,0.5],[-0.4,0.
     # depth_heightmap[depth_heightmap == -z_bottom] = np.nan
     return depth_heightmap
 
+def draw_frame(origin=[0,0,0], q=[0,0,0,1], scale=1):
+    # open3d quaternion format qw qx qy qz
+
+    o3d_quat = np.array([q[3], q[0], q[1], q[2]])
+
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+                     size=scale, origin=origin)
+    frame_rot = copy.deepcopy(mesh_frame).rotate(
+                mesh_frame.get_rotation_matrix_from_quaternion(o3d_quat))
+    
+    return frame_rot
+
 
 
 def main(args):
+    
     plt.ion()
-    if args.color:
-        fig, ax = plt.subplots(3,1)
-    else:
-        fig, ax = plt.subplots(2,1)
-
-    # start from index idx
-    idx = 100
-
-    # check if it is compressed files or not
     flist = os.listdir(args.load_from)
-    if '.npy' in flist[0]:
-        f_format = 'npy'
-    else:
-        f_format = 'npz'
 
-    for i in range(50):
-        fname = 'depth_%07d.'%(idx) + f_format
-        print(fname)
-        depth = np.load(os.path.join(args.load_from, fname))
-        if f_format == 'npz':
-            depth = depth['arr_0']
+    for i in range(10):
+        try:
+            cam_frame = draw_frame(scale=0.3)
 
-        if depth.shape[0] == 720:
-            # camera intrinsics for Realsense D455 at 720 x 1280 resolution
-            K=np.array([[643.014,      0.0,  638.611],
-                        [0.0,      643.014,  365.586],
-                        [0.0,          0.0,    1.0]])
-        elif depth.shape[0] == 480:
-            # camera intrinsics for Realsense D455 at 480 x 848 resolution
-            K=np.array([[425.997,      0.0,  423.08],
-                        [0.0,      425.997,  243.701],
-                        [0.0,          0.0,    1.0]])
+            idx = np.random.randint(len(flist))
 
-        points = compute_point_cloud(depth, K)
-        heightmap = points2heightmap(points, [720, 1280])
+            xyzrgb = np.load(os.path.join(args.load_from, flist[idx])).reshape(-1,6)
+            pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(xyzrgb[:,:3]))
+            pcd.colors = o3d.utility.Vector3dVector(xyzrgb[:,3:].astype('float') / 255)
+            o3d.visualization.draw_geometries([pcd, cam_frame])
 
-        # load color image
-        if args.color:
-            color = np.load(os.path.join(args.load_from, 'color_%07d.npy'%(idx)))
+            pcd = pcd.crop(o3d.geometry.AxisAlignedBoundingBox(np.array([-0.6, -0.2, 2]), 
+                                                               np.array([0.6, 0.35, 4])))
+            o3d.visualization.draw_geometries([pcd, cam_frame])
 
-        ax[0].imshow(depth, vmax=0.8)
-        ax[1].imshow(heightmap)
-        if args.color:
-            ax[2].imshow(color)
+            # cleaned_pcd, _ = pcd.remove_radius_outlier(30, 0.05)
 
-        a = input()
-        idx += 100
-        ax[0].clear()
-        ax[1].clear()
-        if args.color:
-            ax[2].clear()
+            # o3d.visualization.draw_geometries([cleaned_pcd, cam_frame])
 
+            ground_candid = pcd.crop(o3d.geometry.AxisAlignedBoundingBox(np.array([-10, 0.2, -10]), 
+                                                                            np.array([ 10, 0.35, 10])))
+
+            o3d.visualization.draw_geometries([ground_candid, cam_frame])
+
+            plane_model, inliers = ground_candid.segment_plane(distance_threshold=0.03,
+                                                               ransac_n=3,
+                                                               num_iterations=100)
+
+            b_box = pcd.get_axis_aligned_bounding_box()
+            max_bound = b_box.get_max_bound()
+            min_bound = b_box.get_min_bound()
+
+            print('max bound: ', max_bound, 'min_bound: ', min_bound)
+
+    
+        except RuntimeError:
+            print('Failed to find ground')
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test depth filter and projection for corn images')
-    parser.add_argument('--load_from', default='jiacheng/data', help='directory to load images')
+    parser.add_argument('--load_from', default='/home/jc/tmp/front_rgbd', help='directory to load images')
     parser.add_argument('--color', default=0, type=int, help='load color or not')
     args = parser.parse_args()
     
